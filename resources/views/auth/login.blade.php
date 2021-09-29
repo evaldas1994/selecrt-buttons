291dab8325e60e2a3f97c53511af66a23039d129
Nerijus <nerijus@dineta.lt>
2021-09-29 09:08:43 +0300

    Style changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,10 +12,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('User name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="text" class="form-control @error('f_login') is-invalid @enderror" name="f_login" value="{{ old('f_login') }}" required autocomplete="email" autofocus>
+                                <input id="user_name" type="text" class="form-control @error('f_login') is-invalid @enderror" name="f_login" value="{{ old('f_login') }}" required autocomplete="user_name" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -39,6 +39,7 @@
                             </div>
                         </div>
 
+                        <!--
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
@@ -50,18 +51,13 @@
                                 </div>
                             </div>
                         </div>
+                        -->
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
                             </div>
                         </div>
                     </form>
--- resources/views/sale/create.blade.php
+++ resources/views/sale/create.blade.php
@@ -4,8 +4,6 @@
     <div class="container">
         <form method="post" action="{{ route('sales.store') }}">
             @csrf
-
-            <input type="hidden" class="form-control" name="f_op" value="A">
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Docdate</label>
                 <input type="date" class="form-control" name="f_docdate">
@@ -16,8 +14,8 @@
             </div>
 
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Name</label>
-                <input type="text" class="form-control" name="f_name">
+                <label for="exampleInputEmail1" class="form-label">cur</label>
+                <input type="text" class="form-control" name="f_curid">
             </div>
 
 
