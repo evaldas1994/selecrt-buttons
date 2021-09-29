7d8198192ae5d092c8fed8e64c8ed883a50ee8a4
Nerijus <nerijus@dineta.lt>
2021-09-29 09:09:12 +0300

    Added counter helper

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -8,6 +8,14 @@
 if (!function_exists('newrecord')) {
     function newrecord()
     {
-        return collect(\Illuminate\Support\Facades\DB::select('select newrecord() as nv'))->first()->nv;
+        return collect(\Illuminate\Support\Facades\DB::select('select newrecord() as value'))->first()->value;
+    }
+}
+if (!function_exists('counter')) {
+    function counter($counterId)
+    {
+        return collect(
+            \Illuminate\Support\Facades\DB::select('select counter(?) as value', [$counterId])
+        )->first()->value;
     }
 }
