31b0bc2d5661356bcf37830530c7f32070ffd6a9
Evaldas <evaldas.tuleikis@gmail.com>
2021-10-01 15:25:33 +0300

    R8 Crud

---
+++ resources/views/r8/show.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <p>Kodas: {{ $register->f_id }}</p>
+        <p>Pavadinimas: {{ $register->f_name }}</p>
+        <p>Pavadinimas 2: {{ $register->f_name2 }}</p>
+        <p>Galioja iki: {{ $register->f_valid_date }}</p>
+
+        <a href="{{ route('r8s.edit', $register->f_id) }}">Edit</a>
+
+        <form method="post" action="{{ route('r8s.destroy', $register->f_id) }}">
+            @method('DELETE')
+            @csrf
+
+            <button type="submit">Delete</button>
+        </form>
+
+    </div>
+@endsection
