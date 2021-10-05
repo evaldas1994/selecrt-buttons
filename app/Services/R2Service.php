a46e58c0a0ec2b1428551f7d2eeb3c54329d9cf9
Evaldas <evaldas.tuleikis@gmail.com>
2021-10-05 16:39:12 +0300

    Refactor Register2

--- app/Services/R2Service.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Services;
-
-class R2Service extends BaseService
-{
-
-}
---
+++ resources/views/modules/register2/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        @foreach($registers as $register)
+            <p><a href="{{ route('registers2.show', $register->f_id) }}">{{ $register->f_id }} </a></p>
+        @endforeach
+    </div>
+@endsection
