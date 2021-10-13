554283d9046863307931e7cf256fd0fafb5c7318
Evaldas <evaldas.tuleikis@gmail.com>
2021-10-13 16:57:16 +0300

    Refactor Account

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -12,7 +12,7 @@ class Account extends Model
 
     protected $table = 't_account';
 
-    protected $perPage = 10;
+    protected $perPage = 500;
 
     /**
      * The attributes that are mass assignable.
