68a990c5928f83499b3e22a995be53195c1c178d
Evaldas <evaldas.tuleikis@gmail.com>
2021-10-18 11:00:43 +0300

    Manufacturer Crud

---
+++ resources/views/modules/manufacturer/edit.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row mb-2 mb-xl-3">
+        <div class="col-auto">
+            <h1>@lang('modules/manufacturer.h1')</h1>
+        </div>
+
+        <div class="col-auto ms-auto text-end mt-n1">
+            <a href="#" class="btn btn-primary"
+               onclick="event.preventDefault();document.getElementById('manufacturer-form').submit();">@lang('global.btn_save')</a>
+            <a href="{{ route('manufacturers.index') }}" class="btn btn-dark">@lang('global.btn_close')</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="card">
+            <div class="col-12 col-xl-4">
+                <div class="card-body">
+                    <form id="manufacturer-form" action="{{ route('manufacturers.update', $manufacturer) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="mb-2">
+                            <label class="form-label">@lang('modules/manufacturer.f_id')</label>
+                            <input type="text"
+                                   class="not-empty form-control form-control-sm @error('f_id') is-invalid @enderror"
+                                   name="f_id"
+                                   id-pattern
+                                   placeholder="@lang('modules/manufacturer.f_id')"
+                                   required
+                                   maxlength="20"
+                                   value="{{ old('f_id', $manufacturer->f_id) }}">
+                            @error('f_id') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
+                        </div>
+
+                        <div class="mb-2">
+                            <label class="form-label">@lang('modules/manufacturer.f_name')</label>
+                            <input type="text"
+                                   class="form-control form-control-sm @error('f_name') is-invalid @enderror"
+                                   name="f_name"
+                                   placeholder="@lang('modules/manufacturer.f_name')"
+                                   maxlength="100"
+                                   value="{{ old('f_name', $manufacturer->f_name) }}">
+                            @error('f_name') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
+                        </div>
+
+                        <div class="mb-2">
+                            <label class="form-label">@lang('modules/manufacturer.f_name2')</label>
+                            <input type="text"
+                                   class="form-control form-control-sm @error('f_name2') is-invalid @enderror"
+                                   name="f_name2"
+                                   placeholder="@lang('modules/manufacturer.f_name2')"
+                                   maxlength="100"
+                                   value="{{ old('f_name2', $manufacturer->f_name2) }}">
+                            @error('f_name2') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
+                        </div>
+
+                        <div class="mb-2" hidden>
+                            <label class="form-label">@lang('modules/manufacturer.f_system1')</label>
+                            <input type="text"
+                                   class="form-control form-control-sm @error('f_system1') is-invalid @enderror"
+                                   name="f_system1"
+                                   placeholder="@lang('modules/manufacturer.f_system1')"
+                                   maxlength="100"
+                                   value="{{ old('f_system1', $manufacturer->f_system1) }}">
+                            @error('f_system1') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
+                        </div>
+
+                        <div class="mb-2" hidden>
+                            <label class="form-label">@lang('modules/manufacturer.f_system2')</label>
+                            <input type="text"
+                                   class="form-control form-control-sm @error('f_system2') is-invalid @enderror"
+                                   name="f_system2"
+                                   placeholder="@lang('modules/manufacturer.f_system2')"
+                                   maxlength="100"
+                                   value="{{ old('f_system2', $manufacturer->f_system2) }}">
+                            @error('f_system2') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
+                        </div>
+
+                        <div class="mb-2" hidden>
+                            <label class="form-label">@lang('modules/manufacturer.f_system3')</label>
+                            <input type="text"
+                                   class="form-control form-control-sm @error('f_system3') is-invalid @enderror"
+                                   name="f_system3"
+                                   placeholder="@lang('modules/manufacturer.f_system3')"
+                                   maxlength="100"
+                                   value="{{ old('f_system3', $manufacturer->f_system3) }}">
+                            @error('f_system3') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
