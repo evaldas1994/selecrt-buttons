1c8abc2d837a5a4df4f1a189388354baa81e69ab
Nerijus <nerijus@dineta.lt>
2021-10-22 08:50:03 +0300

    Added new blade component limit

---
+++ app/Providers/BladeServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class BladeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blade::directive('limit', function ($value) {
+            return "<?php echo Str::limit($value,40); ?>";
+        });
+    }
+}
