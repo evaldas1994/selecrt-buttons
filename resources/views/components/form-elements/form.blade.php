4d5a3d027d876c5ef68ab041df9bfb3d92026896
Evaldas <evaldas.tuleikis@gmail.com>
2021-11-28 13:45:54 +0200

    fixed forms

--- resources/views/components/form-elements/form.blade.php
+++ resources/views/components/form-elements/form.blade.php
@@ -1,6 +1,7 @@
 <form id="{{ $id }}"
       name="{{ $id }}"
       @isset($data) action="{{ route($route, $data) }}" @else action="{{ route($route) }}" @endisset
+      @isset ($enctype) enctype="{{ $enctype }}" laravel @endisset
       method="POST">
     @csrf
     @method( $method )
