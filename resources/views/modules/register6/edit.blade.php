8e46237bbc0df4993328903c2a98b2c1f8c825f7
Evaldas <evaldas.tuleikis@gmail.com>
2021-12-02 12:10:33 +0200

    refactor register6

--- resources/views/modules/register6/edit.blade.php
+++ resources/views/modules/register6/edit.blade.php
@@ -1,107 +1,110 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row mb-2 mb-xl-3">
-        <div class="col-auto">
-            <h1>@lang('modules/register6.h1')</h1>
+    <div>
+        <div class="row mb-2">
+            <div class="col-auto">
+                <h1>@lang('modules/register6.h1')</h1>
+            </div>
+
+            <div class="col-auto ms-auto text-end mt-n1">
+                <x-form-elements.button
+                    form="register6_edit_form"
+                    class="btn-primary"
+                    text="global.btn_save"
+                />
+
+                <x-form-elements.button
+                    form="register6_edit_form"
+                    class="btn-dark"
+                    name="button-action-without-validation"
+                    value="close"
+                    text="global.btn_close"
+                />
+            </div>
         </div>
 
-        <div class="col-auto ms-auto text-end mt-n1">
-            <a href="#" class="btn btn-primary"
-               onclick="event.preventDefault();document.getElementById('register6-form').submit();">@lang('global.btn_save')</a>
-            <a href="{{ route('registers6.index') }}" class="btn btn-dark">@lang('global.btn_close')</a>
-        </div>
-    </div>
-    <div class="row">
-        <div class="card">
-            <div class="col-12 col-xl-4">
-                <div class="card-body">
-                    <form id="register6-form" action="{{ route('registers6.update', $registers6) }}" method="POST">
-                        @csrf
-                        @method('PUT')
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12 col-md-6 col-xl-3">
+                                <x-form-elements.input-id
+                                    form="register6_edit_form"
+                                    name="f_id"
+                                    labelValue="modules/register6.f_id"
+                                    maxLength="20"
+                                    inputClass="not-empty"
+                                    :defaultValue="$registers6->f_id"
+                                />
+                            </div>
+                            <div class="col-12 col-md-6 col-xl-3">
+                                <x-form-elements.input
+                                    form="register6_edit_form"
+                                    name="f_name"
+                                    labelValue="modules/register6.f_name"
+                                    maxLength="100"
+                                    :defaultValue="$registers6->f_name"
+                                />
+                            </div>
+                            <div class="col-12 col-md-6 col-xl-3">
+                                <x-form-elements.input
+                                    form="register6_edit_form"
+                                    name="f_name2"
+                                    labelValue="modules/register6.f_name2"
+                                    maxLength="100"
+                                    :defaultValue="$registers6->f_name2"
+                                />
+                            </div>
+                            <div class="col-12 col-md-6 col-xl-3">
+                                <x-form-elements.input-date
+                                    form="register6_edit_form"
+                                    name="f_valid_date"
+                                    labelValue="modules/register6.f_valid_date"
+                                    :defaultValue="$registers6->f_valid_date"
+                                />
+                            </div>
+                            <div class="col-12 col-md-6 col-xl-3">
+                                <x-form-elements.input
+                                    form="register6_edit_form"
+                                    name="f_system1"
+                                    labelValue="modules/register6.f_system1"
+                                    maxLength="100"
+                                    hidden="hidden"
+                                    :defaultValue="$registers6->f_system1"
+                                />
 
-                        <div class="mb-2">
-                            <label class="form-label">@lang('modules/register6.f_id')</label>
-                            <input type="text"
-                                   class="not-empty form-control form-control-sm @error('f_id') is-invalid @enderror"
-                                   name="f_id"
-                                   id-pattern
-                                   placeholder="@lang('modules/register6.f_id')"
-                                   required
-                                   maxlength="20"
-                                   value="{{ old('f_id', $registers6->f_id) }}">
-                            @error('f_id') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
+                                <x-form-elements.input
+                                    form="register6_edit_form"
+                                    name="f_system2"
+                                    labelValue="modules/register6.f_system2"
+                                    maxLength="100"
+                                    hidden="hidden"
+                                    :defaultValue="$registers6->f_system2"
+                                />
+
+                                <x-form-elements.input
+                                    form="register6_edit_form"
+                                    name="f_system3"
+                                    labelValue="modules/register6.f_system3"
+                                    maxLength="100"
+                                    hidden="hidden"
+                                    :defaultValue="$registers6->f_system3"
+                                />
+                            </div>
                         </div>
-
-                        <div class="mb-2">
-                            <label class="form-label">@lang('modules/register6.f_name')</label>
-                            <input type="text"
-                                   class="form-control form-control-sm @error('f_name') is-invalid @enderror"
-                                   name="f_name"
-                                   placeholder="@lang('modules/register6.f_name')"
-                                   maxlength="100"
-                                   value="{{ old('f_name', $registers6->f_name) }}">
-                            @error('f_name') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
-                        </div>
-
-                        <div class="mb-2">
-                            <label class="form-label">@lang('modules/register6.f_name2')</label>
-                            <input type="text"
-                                   class="form-control form-control-sm @error('f_name2') is-invalid @enderror"
-                                   name="f_name2"
-                                   placeholder="@lang('modules/register6.f_name2')"
-                                   maxlength="100"
-                                   value="{{ old('f_name2', $registers6->f_name2) }}">
-                            @error('f_name2') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
-                        </div>
-
-                        <div class="mb-2">
-                            <label class="form-label">@lang('modules/register6.f_valid_date')</label>
-                            <input
-                                type="text"
-                                class="form-control form-control-sm date"
-                                name="f_valid_date"
-                                placeholder="@lang('global.select_date')"
-                                value="{{ old('f_valid_date', $registers6->f_valid_date) }}">
-                            @error('f_valid_date') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
-                        </div>
-
-                        <div class="mb-2" hidden>
-                            <label class="form-label">@lang('modules/register6.f_system1')</label>
-                            <input type="text"
-                                   class="form-control form-control-sm @error('f_system1') is-invalid @enderror"
-                                   name="f_system1"
-                                   placeholder="@lang('modules/register6.f_system1')"
-                                   maxlength="100"
-                                   value="{{ old('f_system1', $registers6->f_system1) }}">
-                            @error('f_system1') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
-                        </div>
-
-                        <div class="mb-2" hidden>
-                            <label class="form-label">@lang('modules/register6.f_system2')</label>
-                            <input type="text"
-                                   class="form-control form-control-sm @error('f_system2') is-invalid @enderror"
-                                   name="f_system2"
-                                   placeholder="@lang('modules/register6.f_system2')"
-                                   maxlength="100"
-                                   value="{{ old('f_system2', $registers6->f_system2) }}">
-                            @error('f_system2') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
-                        </div>
-
-                        <div class="mb-2" hidden>
-                            <label class="form-label">@lang('modules/register6.f_system3')</label>
-                            <input type="text"
-                                   class="form-control form-control-sm @error('f_system3') is-invalid @enderror"
-                                   name="f_system3"
-                                   placeholder="@lang('modules/register6.f_system3')"
-                                   maxlength="100"
-                                   value="{{ old('f_system3', $registers6->f_system3) }}">
-                            @error('f_system3') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
-                        </div>
-
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
+
+        {{--    Forms--}}
+        <x-form-elements.form
+            id="register6_edit_form"
+            route="registers6.update"
+            :data="$registers6"
+            method="PUT"
+        />
     </div>
 @endsection
