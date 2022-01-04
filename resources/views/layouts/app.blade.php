566de7dcf158422ad7b80d4711738dba140b8bb3
evaldas1994 <evaldas.tuleikis@gmail.com>
2022-01-04 14:00:09 +0200

    Added grid columns select

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,6 @@
     @include('layouts.main')
 @endif
 @livewireScripts
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="{{ asset('js/resizable-table-columns/index.min.js') }}"></script>
 <script src="{{ asset('js/resizable-table-columns/store.js') }}"></script>
 <script src="{{ asset('theme/js/app.js') }}"></script>
