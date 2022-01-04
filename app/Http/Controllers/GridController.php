d38c43068871c7a9a92e2ad686237a0156b6b559
evaldas1994 <evaldas.tuleikis@gmail.com>
2022-01-04 13:29:59 +0200

    Added grid columns select

--- app/Http/Controllers/GridController.php
+++ app/Http/Controllers/GridController.php
@@ -3,22 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Grid;
-use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
-use App\Models\ProductionCard;
 use Illuminate\Support\Facades\URL;
 
 class GridController extends Controller
 {
     public function saveActiveColumns(Request $request)
     {
-        $message = 'global.reseted_successfully';
+        $message = 'grid.saved_successfully';
         $data = $this->getData($request);
 
         try {
             $this->save($data);
         } catch (\Throwable $exception) {
-            $message = 'global.reset_failed';
+            $message = 'grid.reset_failed';
+            return redirect()->back()->withSuccess(trans($message));
         }
 
         return redirect()->back()->withSuccess(trans($message));
@@ -26,7 +25,7 @@ class GridController extends Controller
 
     public function resetActiveColumns()
     {
-        $message = 'global.reseted_successfully';
+        $message = 'grid.reseted_successfully';
         $data = $this->getData();
 
         try {
@@ -51,30 +50,9 @@ class GridController extends Controller
         return [
             'f_userid' => auth()->user()->f_id,
             'f_form' => URL::previous(),
-            'f_col_sel' => $this->cleaned($request)
+            'f_col_sel' => $request->get('columns') === null || json_decode($request->input('columns') === null)
+                    ? null
+                    : json_decode($request->input('columns'))
         ];
-    }
-
-    private function cleaned($request)
-    {
-        $gridColumns = ProductionCard::$gridColumns;
-        $defaultGridColumns = ProductionCard::$defaultGridColumns;
-
-        $columns = json_decode($request->input('columns'));
-//        $columns = Arr::add($columns, count($columns), 'bam');
-
-        if($columns !== null && $columns !== []) {
-            foreach ($columns as $key => $column) {
-                if(array_search($column, $gridColumns) === false) {
-                    Arr::forget($columns, $key);
-                }
-            }
-        }
-
-        if (json_encode($columns) === null || $columns === null) {
-            return json_encode($defaultGridColumns);
-        }
-
-        return json_encode($columns);
     }
 }
---
+++ resources/lang/lt/grid.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by
+ * User: Nerijus Bartoševičius
+ * Date: 2021-10-12
+ */
+
+
+return [
+    'saved_successfully' => 'Stulpelių išdėstymas išsaugotas sėkmingai!',
+    'reseted_successfully' => 'Stulpelių išdėstymas atstatytas sėkmingai!',
+    'reset_failed' => 'Stulpelių išdėstymo atstatyti negalima!',
+    'update_failed' => 'Stulpelių išdėstymo atnaujinti negalima!',
+];
