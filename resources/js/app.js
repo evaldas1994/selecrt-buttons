9adaccc25c651097f34a08b75e6c442ba67f8c26
Nerijus <nerijus@dineta.lt>
2022-01-05 09:26:30 +0200

    Added vue js 3 library
    Added vue js 3 library and removed unused reactjs lib

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,10 +6,17 @@
 
 require('./bootstrap');
 
+import { createApp } from 'vue';
+import Welcome from './components/Welcome.vue';
+const app = createApp({});
+app.component('welcome', Welcome);
+app.mount('#app');
+
+
 /**
  * Next, we will create a fresh React component instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-require('./components/Example');
+//require('./components/Example');
---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+<script>
+export default {
+    setup: () => ({
+        title: 'Welcome to Vue JS 3!!!'
+    })
+}
+
+</script>
